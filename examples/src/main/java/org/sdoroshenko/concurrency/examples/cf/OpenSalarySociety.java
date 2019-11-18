8ea7123749d2cc25ef8748aebd92f1817a1bb31e
Sergei Doroshenko <sdoroshenko@edmunds.com>
2019-11-18 20:10:01 +0300

    Java Concurrency: CompletableFuture samples

--- examples/src/main/java/org/sdoroshenko/concurrency/examples/cf/OpenSalarySociety.java
+++ examples/src/main/java/org/sdoroshenko/concurrency/examples/cf/OpenSalarySociety.java
@@ -1,4 +1,17 @@
 package org.sdoroshenko.concurrency.examples.cf;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class OpenSalarySociety {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        int result = CompletableFuture.supplyAsync(() -> "Hello").thenApply(str -> str.length())
+            .thenCombine(
+                CompletableFuture.supplyAsync(() -> " World").thenApply(str -> str.length()), (s1, s2) -> s1 + s2
+            ).join();
+
+        // thenCombine
+        System.out.println(result);
+    }
 }
